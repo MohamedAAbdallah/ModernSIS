3dced681526b94d0d16e3183b4d66dee824bfedb
Mohamed A. Abdallah <eng.mohamed.a.abdallah@gmail.com>
2024-10-03 10:25:51 +0300

    prettier

--- src/localize.js
+++ src/localize.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('selectTheme').innerText = chrome.i18n.getMessage('selectTheme');
-    document.getElementById('githubStar').innerText = chrome.i18n.getMessage('githubStar');
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("selectTheme").innerText =
+    chrome.i18n.getMessage("selectTheme");
+  document.getElementById("githubStar").innerText =
+    chrome.i18n.getMessage("githubStar");
 });
