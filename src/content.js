e48ea9d78e8ff2ee24bbf76580fa78d7976631d4
Mohamed A. Abdallah <eng.mohamed.a.abdallah@gmail.com>
2024-10-21 09:46:11 +0300

    Updated content.js

--- src/content.js
+++ src/content.js
@@ -1,29 +1,27 @@
 function applyTheme(theme) {
+  const id = chrome.i18n.getMessage("@@extension_id");
+  const imgBanner = document.getElementById("HeaderReg_imgBanner");
+
   if (theme === "None") {
     document.body.id = "";
     document.body.className = "";
-
-    document.getElementById("HeaderReg_imgBanner").src = "images/header-5.jpg";
+    imgBanner.src = "images/header-5.jpg";
   } else {
-    document.body.id = `${theme}`;
-    document.body.className = `ModernSIS`;
-
-    const id = chrome.i18n.getMessage("@@extension_id");
-    const path = `chrome-extension://${id}/imgs/banners/${theme}.jpg`;
-    document.getElementById("HeaderReg_imgBanner").src = path;
+    document.body.id = theme;
+    document.body.className = "ModernSIS";
+    imgBanner.src = `chrome-extension://${id}/imgs/banners/${theme}.jpg`;
   }
 }
 
 chrome.storage.local.get("theme", (data) => {
-  const theme = data.theme || "None";
-  applyTheme(theme);
+  applyTheme(data.theme || "None");
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "changeTheme") {
-    const theme = request.theme;
-    chrome.storage.local.set({ theme: theme }, () => {
-      applyTheme(theme);
+    const newTheme = request.theme;
+    chrome.storage.local.set({ theme: newTheme }, () => {
+      applyTheme(newTheme);
     });
   }
 });
